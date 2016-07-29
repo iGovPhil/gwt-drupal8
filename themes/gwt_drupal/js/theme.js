8995dcb7b8878c33ad6bda3a32a1fe3c5117c1e2
Voj Jeturian <jeturian@gmail.com>
2016-07-29 10:55:57 +0800

    initialize Drupal 8, finish basic page structure and regions

---
+++ themes/gwt_drupal/js/theme.js
@@ -0,0 +1,136 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ */
+ 
+// Cookie handler, native style
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else
+        var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    // createCookie(name, "", -1);
+    createCookie(name, "");
+}
+
+(function ($, Drupal, window, document, undefined) {
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+    $(document).foundation();
+
+    // add a place holder for the search text field
+    $('#search-block-form .form-search').attr('placeholder', 'Search...');
+    return false;
+
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('#back-to-top').fadeIn(duration);
+        } else {
+            $('#back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('#back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+
+    var mainPosition = $('#main').position();
+    var menuHeight = $('#top-menu .sticky').height();
+    var footerPosition = $('#footer').position();
+    $(window).resize(function() {
+        mainPosition = $('#main').position();
+        footerPosition = $('#footer').position();
+    });
+    $('.a11y-skip-to-content').click(function(event) {
+        event.preventDefault();
+        $('#offCanvas').foundation('close');
+        $('html, body').animate({scrollTop: mainPosition.top-menuHeight}, duration);
+        return false;
+    });
+    $('.a11y-skip-to-footer').click(function(event) {
+        event.preventDefault();
+        $('#offCanvas').foundation('close');
+        $('html, body').animate({scrollTop: footerPosition.top-menuHeight}, duration);
+        return false;
+    });
+
+    if(!Drupal.settings.hasOwnProperty('gwt_drupal')){
+        return false;
+    }
+    if(!Drupal.settings.gwt_drupal.hasOwnProperty('theme_path')){
+        return false; // a simple validation catch for undefined variable.
+    }
+
+    var a11y_stylesheet_path = Drupal.settings.gwt_drupal.theme_path+'/accessibility/a11y-contrast.css';
+    if (readCookie('a11y-high-contrast')) {
+        $('body').addClass('contrast');
+        $('head').append($("<link href='" + a11y_stylesheet_path + "' id='highContrastStylesheet' rel='stylesheet' type='text/css' />"));
+        $('.a11y-toggle-contrast').attr('aria-checked', true).addClass('active');
+        $('.a11y-toolbar ul li a i').addClass('icon-white');
+    }
+    $('.a11y-toggle-contrast').on('click', function () {
+    var a11y_stylesheet_path = Drupal.settings.gwt_drupal.theme_path+'/accessibility/a11y-contrast.css';
+        if (!$(this).hasClass('active')) {
+            $('head').append($("<link href='" + a11y_stylesheet_path + "' id='highContrastStylesheet' rel='stylesheet' type='text/css' />"));
+            $('body').addClass('contrast');
+            // $(this).parent().parent().find('i').addClass('icon-white');
+            createCookie('a11y-high-contrast', '1');
+            $(this).attr('aria-checked', true).addClass('active');
+            return false;
+        }
+        else {
+            $('#highContrastStylesheet').remove();
+            $('body').removeClass('contrast');
+            $(this).removeAttr('aria-checked').removeClass('active');
+            // $(this).parent().parent().find('i').removeClass('icon-white');
+            eraseCookie('a11y-high-contrast');
+            return false;
+        }
+    });
+    // Saturation handler
+    var a11y_desaturate_path = Drupal.settings.gwt_drupal.theme_path+'/accessibility/a11y-desaturate.css';
+    if(readCookie('a11y-desaturated')) {
+        $('body').addClass('desaturated');
+        $('head').append($("<link href='" + a11y_desaturate_path + "' id='desaturateStylesheet' rel='stylesheet' type='text/css' />"));
+        $('.a11y-toggle-grayscale').attr('aria-checked', true).addClass('active');
+    };
+    
+    /*$('.a11y-toggle-grayscale').on('click', function(){
+        if(!$(this).hasClass('active')){
+            $('head').append($("<link href='" + a11y_desaturate_path + "' id='desaturateStylesheet' rel='stylesheet' type='text/css' />"));
+            $('body').addClass('desaturated');
+            $(this).attr('aria-checked', true).addClass('active');
+            createCookie('a11y-desaturated', '1');
+            return false;
+        } else {
+            $('#desaturateStylesheet').remove();
+            $('body').removeClass('desaturated');
+            $(this).removeAttr('aria-checked').removeClass('active');
+            eraseCookie('a11y-desaturated');
+            return false;
+        }
+    });*/
+  }
+};
+})(jQuery, Drupal, this, this.document);
